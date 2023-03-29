93583733fdf9bcd38295799f02140e971781e121
mpavle1 <102835309+mpavle1@users.noreply.github.com>
2023-03-29 03:02:21 +0200

    added users and auth

---
+++ server/src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post, Body } from '@nestjs/common/decorators';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
+  }
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+}
---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+
+@Injectable()
+export class AuthService {
+  constructor(private prismaService: PrismaService) {}
+
+  async login(dto: AuthDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    return {
+      message: 'Login',
+    };
+  }
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    let user = null;
+    try {
+      user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
+
+    delete user.hash;
+
+    return user;
+  }
+}
